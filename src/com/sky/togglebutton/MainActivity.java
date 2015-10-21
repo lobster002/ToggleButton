8bef54e3e02f3204360fea1a941317910aef5d2c
lobster002 <752996878@qq.com>
2015-10-21 22:00:12 +0800

    修正一些Bug，可以自定义尺寸以及图片资源

--- src/com/sky/togglebutton/MainActivity.java
+++ src/com/sky/togglebutton/MainActivity.java
@@ -3,12 +3,18 @@ package com.sky.togglebutton;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import com.sky.togglebutton.R;
 import com.sky.togglebutton.ToggleButton.ToggleState;
+import com.sky.togglebutton.ToggleButton.onToggleStateChangeListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements
+		onToggleStateChangeListener {
 
 	private ToggleButton btn = null;
+	private TextView tv = null;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +22,25 @@ public class MainActivity extends Activity {
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_main);
+		
+		tv = (TextView) findViewById(R.id.tv);
 
-		btn = (ToggleButton) findViewById(R.id.btn);
-		btn.setBackgroundResource(R.drawable.bg);
-		btn.setSwitchBackgroundResource(R.drawable.btnbg);
-		btn.setToggleState(ToggleState.CLOSE);
+		btn = (ToggleButton) findViewById(R.id.btn);// 取得控件
+		btn.setBackgroundResource(R.drawable.bg);// 设置开关打开时背景状态
+		btn.setCloseStateBackgroundResource(R.drawable.bg_2);// 设置开关关闭时背景
+		btn.setSwitchBackgroundResource(R.drawable.btnbg);// 设置开关滑块
+		btn.setToggleState(ToggleState.OPEN);// 设置默认状态
+
+		btn.setToggleStateChangeListener(this);
+	}
+
+	@Override
+	public void onToggleStateChanged(ToggleState state) {
+		if (state == ToggleState.OPEN) {
+			tv.setText("开");
+		} else {
+			tv.setText("关");
+		}
 	}
 
 }
--- src/com/sky/togglebutton/ToggleButton.java
+++ src/com/sky/togglebutton/ToggleButton.java
@@ -4,30 +4,117 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
-public class ToggleButton extends View {
+public  class ToggleButton extends View {
 
 	private ToggleState mToggleState = ToggleState.OPEN;
 	private Bitmap slidBg = null;
-	private Bitmap bg = null;
+	private Bitmap slidBg_close = null;
+	private Bitmap switchBg = null;
+	private int left = 0;
+	private int centerX = 0;
+	private int currentX = 0;
+	private boolean isSliding = false;
+	private onToggleStateChangeListener listener = null;
+
+	private int Height = 0;
+	private int Width = 0;
+
+	private Bitmap reSetBitmap(Bitmap bp, boolean isSwitch) {// 重新计算生成Bitmap
+		// 原Bitmap 尺寸
+		int bpWidth = bp.getWidth();
+		int bpHeight = bp.getHeight();
+
+		// 缩放比例
+		float widthScale = Width * 1.0f / bpWidth;
+		float heightScale = Height * 1.0f / bpHeight;
+
+		Matrix matrix = new Matrix();
+		if (isSwitch) {// 如果是滑块 缩放比例为高度
+			matrix.postScale(heightScale, heightScale);
+		} else {// 如果是背景
+			matrix.postScale(widthScale, heightScale);
+		}
+		Bitmap bitmap = Bitmap.createBitmap(bp, 0, 0, bp.getWidth(),
+				bp.getHeight(), matrix, false);
+		bp.recycle();// 回收原Bitmap
+		return bitmap;
+	}
 
 	public ToggleButton(Context context, AttributeSet attrs) {
 		// 在xml布局中使用时，需要改方法
 		super(context, attrs);
 	}
 
+	public void setCloseStateBackgroundResource(int id) {// 根据传递的id 加载Bitmap
+		slidBg_close = BitmapFactory.decodeResource(getResources(), id);
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent event) {// 自定义Touch事件
+		if (null == switchBg) {// 至少要有滑块图片
+			return true;
+		}
+		currentX = (int) event.getX();// 当前手指的位置
+
+		switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:// 摁下时
+			isSliding = true;// 是否正在滑动 是
+			break;
+		case MotionEvent.ACTION_UP:// 抬起时
+			isSliding = false;// 否
+
+			if (currentX > centerX) {
+				if (ToggleState.OPEN != mToggleState) {
+					mToggleState = ToggleState.OPEN;
+					if (null != listener) {// 回调
+						listener.onToggleStateChanged(mToggleState);
+					}
+				}
+			} else {
+				if (ToggleState.CLOSE != mToggleState) {
+					mToggleState = ToggleState.CLOSE;
+					if (null != listener) {// 回调
+						listener.onToggleStateChanged(mToggleState);
+					}
+				}
+			}
+			break;
+		}
+
+		invalidate();// 更新
+		return true;
+	}
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-		if (null == bg) {
+		if (null == switchBg) {// 至少需要有滑块图片
 			return;
 		}
-		setMeasuredDimension(bg.getWidth(), bg.getHeight());
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+		
+		Height = getMeasuredHeight();//获取测量完毕后控件的高
+		Width = getMeasuredWidth();//获取测量完毕后控件的宽
+		centerX = Width / 2;// 控件中心
+		
+		// 重新计算Bitmap的尺寸 适配控件（如果设置的话）
+		if (null != slidBg) {
+			slidBg = reSetBitmap(slidBg, false);
+		}
+		if (null != slidBg_close) {
+			slidBg_close = reSetBitmap(slidBg_close, false);
+		}
+		if (null != switchBg) {
+			switchBg = reSetBitmap(switchBg, true);
+		}
 	}
 
 	public void setBackgroundResource(int id) {
-		bg = BitmapFactory.decodeResource(getResources(), id);
+		slidBg = BitmapFactory.decodeResource(getResources(), id);
 	}
 
 	public enum ToggleState {
@@ -39,25 +126,43 @@ public class ToggleButton extends View {
 	}
 
 	public void setSwitchBackgroundResource(int id) {
-		slidBg = BitmapFactory.decodeResource(getResources(), id);
+		switchBg = BitmapFactory.decodeResource(getResources(), id);
 	}
 
 	@Override
 	protected void onDraw(Canvas canvas) {
-		
-		canvas.drawBitmap(bg, 0, 0, null);
-		
-		switch (mToggleState) {
-		
-			case CLOSE:
-				canvas.drawBitmap(slidBg, 0, 0, null);
-				break;
-				
-			case OPEN:
-				canvas.drawBitmap(slidBg, bg.getWidth() - slidBg.getWidth(), 0, null);
-			break;
+		if (null == switchBg) {
+			return;
 		}
 
+		if (ToggleState.CLOSE == mToggleState && null != slidBg_close) {// 如果设置了关闭时背景
+																		// 且当前状态为关闭
+			canvas.drawBitmap(slidBg_close, 0, 0, null);
+		} else if (null != slidBg) {// 如果状态为开 且设置有打开的图片
+			canvas.drawBitmap(slidBg, 0, 0, null);
+		}
+
+		left = currentX - switchBg.getWidth() / 2;// 绘制从左边开始的位置
+
+		if (isSliding) {// 如果正在滑动 修正位置
+			if (left < 0) {
+				left = 0;
+			}
+			if (left > (Width - switchBg.getWidth())) {
+				left = Width - switchBg.getWidth();
+			}
+			canvas.drawBitmap(switchBg, left, 0, null);
+		} else { // 否则根据开闭状态 绘制
+			switch (mToggleState) {
+			case CLOSE:
+				canvas.drawBitmap(switchBg, 0, 0, null);
+				break;
+			case OPEN:
+				canvas.drawBitmap(switchBg, Width - switchBg.getWidth(), 0,
+						null);
+				break;
+			}
+		}
 	}
 
 	public ToggleButton(Context context) {
@@ -65,4 +170,12 @@ public class ToggleButton extends View {
 		super(context);
 	}
 
+	public void setToggleStateChangeListener(
+			onToggleStateChangeListener listener) {
+		this.listener = listener;
+	}
+
+	public interface onToggleStateChangeListener {
+		void onToggleStateChanged(ToggleState state);
+	}
 }
